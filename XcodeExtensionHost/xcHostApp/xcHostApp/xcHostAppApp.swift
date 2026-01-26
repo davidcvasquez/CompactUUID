122b2f1dba8f058ca1222b9f5ff2f945a844cf4d
David Vasquez <david.charles.vasquez@gmail.com>
2026-01-26 07:06:32 -0800

    Updated license headers for xc plugin

--- XcodeExtensionHost/xcHostApp/xcHostApp/xcHostAppApp.swift
+++ XcodeExtensionHost/xcHostApp/xcHostApp/xcHostAppApp.swift
@@ -1,9 +1,13 @@
+//===----------------------------------------------------------------------===//
 //
-//  xcHostAppApp.swift
-//  xcHostApp
+// This source file is part of the CompactUUID open source project
 //
-//  Created by David Vasquez on 1/26/26.
+// Copyright (c) 2026 David C. Vasquez and the CompactUUID project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
+// See the project's LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
 
 import SwiftUI
 
